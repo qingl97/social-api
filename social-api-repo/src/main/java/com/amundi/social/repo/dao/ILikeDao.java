b136487db86536272c027473d4558b8ec2998e71
Qing LIANG <qing.liang-ext@amundi.com>
2016-05-13 18:50:17 +0200

    add LikeDao interface, declared it's implementation class

--- social-api-repo/src/main/java/com/amundi/social/repo/dao/ILikeDao.java
+++ social-api-repo/src/main/java/com/amundi/social/repo/dao/ILikeDao.java
@@ -1,6 +1,19 @@
 package com.amundi.social.repo.dao;
 
+import java.util.List;
+import java.util.Map;
+
+import com.amundi.social.common.model.Like;
+
 public interface ILikeDao {
 
+	List<Like> getAll();
+	List<Like> getLikesByUser(String userId);
+	List<Like> getLikesByApplication(String appId);
+	List<Like> getLikesByProduct(String appId, String productId);
 	
+	void add(String appId, String productId, String userId);
+	void delete(String appId, String productId, String userId);
+	
+	Map<String, List<Like>> getLikesByUsers(List<String> userIds); 
 }
--- social-api-repo/src/main/java/com/amundi/social/repo/dao/impl/LikeDaoImpl.java
+++ social-api-repo/src/main/java/com/amundi/social/repo/dao/impl/LikeDaoImpl.java
@@ -1,11 +1,51 @@
 package com.amundi.social.repo.dao.impl;
 
+import java.util.List;
+import java.util.Map;
+
+import com.amundi.social.common.model.Like;
 import com.amundi.social.repo.dao.ILikeDao;
 
 public class LikeDaoImpl implements ILikeDao {
 
-	public LikeDaoImpl() {
+	@Override
+	public List<Like> getLikesByUser(String userId) {
+		return null;
+	}
+
+	@Override
+	public void add(String appId, String productId, String userId) {
 		
+	}
+
+	@Override
+	public void delete(String appId, String productId, String userId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Map<String, List<Like>> getLikesByUsers(List<String> userIds) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Like> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Like> getLikesByApplication(String appId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Like> getLikesByProduct(String appId, String productId) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
