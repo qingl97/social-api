9c3f859e459739eee2ceadbf3df1a1552d580348
Qing LIANG <qing.liang-ext@amundi.com>
2016-05-20 16:50:29 +0200

    common resource

---
+++ social-api-war/src/main/java/com/amundi/social/rest/resources/AbstractCommonResource.java
@@ -0,0 +1,93 @@
+package com.amundi.social.rest.resources;
+
+import java.util.List;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import org.apache.log4j.Logger;
+
+import com.amundi.social.common.model.IAction.ActionType;
+import com.amundi.social.common.providers.IProductProvider;
+import com.amundi.social.common.providers.IUserActivityProvider;
+import com.amundi.social.core.providers.impl.ProductService;
+import com.amundi.social.core.providers.impl.UserActivityService;
+import com.amundi.social.rest.resources.util.JsonUtil;
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import net.active.services.server.util.AbstractResource;
+
+public abstract class AbstractCommonResource extends AbstractResource implements ICommonResource {
+	
+	private static final Logger LOGGER = Logger.getLogger(AbstractCommonResource.class);
+	
+	private ActionType type;
+	private IUserActivityProvider userActivityService = new UserActivityService();
+	private IProductProvider productService = new ProductService();
+	
+	public AbstractCommonResource(ActionType type) {
+		this.type = type;
+	}
+
+	@Override
+	public Response getAll(boolean detail) {
+		List<? extends Object> ret;
+		ret = detail ? userActivityService.get(type) : productService.getAll();
+		return buildDefaultJsonFormatResponse(ret);
+	}
+
+	@Override
+	public Response getUserActivities(String userId) {
+		return buildDefaultJsonFormatResponse(userActivityService.get(userId, type));
+	}
+
+	@Override
+	public Response getUserActivities(String userId, String appId) {
+		return buildDefaultJsonFormatResponse(userActivityService.get(userId, appId, type));
+	}
+
+	@Override
+	public Response getUserActivities(String userId, String appId, String productId) {
+		return buildDefaultJsonFormatResponse(userActivityService.get(userId, appId, productId, type));
+	}
+
+	@Override
+	public Response doAction(String userId, String appId, String productId) {
+		userActivityService.doAction(userId, appId, productId, type);
+		LOGGER.info("user " + userId + " does " + type.toString() + " on product " + productId + " of application " + appId);
+		return Response.ok().build();
+	}
+
+	@Override
+	public Response undoAction(String userId, String appId, String productId) {
+		userActivityService.undoAction(userId, appId, productId, type);
+		LOGGER.info("user " + userId + " undo " + type.toString() + " on product " + productId + " of application " + appId);
+		return Response.ok().build();
+	}
+
+	@Override
+	public Response getByApplication(String appId, boolean detail) {
+		List<? extends Object> activities = 
+				detail == true ? productService.getWithDetails(appId, type) : productService.get(appId);
+		return buildDefaultJsonFormatResponse(activities);
+	}
+
+	@Override
+	public Response getByProduct(String appId, String productId, boolean detail) {
+		if(detail) {
+			return buildDefaultJsonFormatResponse(productService.getWithDetails(appId, productId, type));
+		} else {
+			return buildDefaultJsonFormatResponse(productService.get(appId, productId));
+		}
+	}
+	
+	protected Response buildDefaultJsonFormatResponse(Object value) {
+		try {
+			return buildDefaultResponse(JsonUtil.serialize(value));
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+			return Response.status(Status.INTERNAL_SERVER_ERROR).entity("Failed to serialize object to JSON format").build();
+		}
+	}
+
+}
---
+++ social-api-war/src/main/java/com/amundi/social/rest/resources/ICommonResource.java
@@ -0,0 +1,44 @@
+package com.amundi.social.rest.resources;
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+
+public interface ICommonResource {
+
+	@GET
+	public Response getAll(@QueryParam("detail") boolean detail);
+	
+	@GET 
+	@Path("/user/{user_id}")
+	public Response getUserActivities(@PathParam("user_id") String userId);
+	
+	@GET 
+	@Path("/user/{user_id}/app/{app_id}")
+	public Response getUserActivities(@PathParam("user_id") String userId, @PathParam("app_id") String appId);
+	
+	@GET
+	@Path("/user/{user_id}/app/{app_id}/product/{product_id}")
+	public Response getUserActivities(@PathParam("user_id") String userId, @PathParam("app_id") String appId, @PathParam("product_id") String productId);
+	
+	@PUT
+	@Path("/user/{user_id}/app/{app_id}/product/{product_id}")
+	public Response doAction(@PathParam("user_id") String userId, @PathParam("app_id") String appId, @PathParam("product_id") String productId);
+	
+	@DELETE
+	@Path("/user/{user_id}/app/{app_id}/product/{product_id}")
+	public Response undoAction(@PathParam("user_id") String userId, @PathParam("app_id") String appId, @PathParam("product_id") String productId);
+	
+	@GET
+	@Path("/app/{app_id}")
+	public Response getByApplication(@PathParam("app_id") String appId, @QueryParam("detail") boolean detail);
+	
+	@GET
+	@Path("/app/{app_id}/product/{product_id}")
+	public Response getByProduct(@PathParam("app_id") String appId, @PathParam("product_id") String productId, @QueryParam("detail") boolean detail);
+	
+}
