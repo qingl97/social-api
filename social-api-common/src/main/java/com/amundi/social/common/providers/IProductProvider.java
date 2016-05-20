bfb119b04fdc8cdf65e442211d1dcdd17517d405
Qing LIANG <qing.liang-ext@amundi.com>
2016-05-20 16:49:58 +0200

    refac

--- social-api-common/src/main/java/com/amundi/social/common/providers/IProductProvider.java
+++ social-api-common/src/main/java/com/amundi/social/common/providers/IProductProvider.java
@@ -3,15 +3,18 @@ package com.amundi.social.common.providers;
 import java.util.List;
 import java.util.Map;
 
-import com.amundi.social.common.model.AbstractAction;
+import com.amundi.social.common.model.IAction;
 import com.amundi.social.common.model.IAction.ActionType;
 import com.amundi.social.common.model.IProduct;
 
 public interface IProductProvider {
 
 	List<IProduct> getAll();
+	List<IProduct> getAll(ActionType type);
 	List<IProduct> get(String appId);
 	IProduct get(String appId, String productId);
-	Map<ActionType, List<? extends AbstractAction>> getSocialStatus(String appId, String productId);
-	List<? extends AbstractAction> getSocialStatus(String appId, String productId, ActionType type);
+	Map<ActionType, List<? extends IAction>> getWithDetails(String appId, String productId);
+	List<? extends IAction> getWithDetails(String appId, String productId, ActionType type);
+	Map<ActionType, List<? extends IAction>> getWithDetails(String appId);
+	List<? extends IAction> getWithDetails(String appId, ActionType type);
 }
