05145cc019765629602192fc7b14d69738e42cbf
Qing LIANG <qing.liang-ext@amundi.com>
2016-05-30 15:45:02 +0200

    changed WS resource /api/products/all to /api/products

--- social-api-war/src/main/java/com/amundi/social/rest/resources/ProductViewResource.java
+++ social-api-war/src/main/java/com/amundi/social/rest/resources/ProductViewResource.java
@@ -11,7 +11,7 @@ import javax.ws.rs.core.Response;
 @Produces(MediaType.APPLICATION_JSON)
 public interface ProductViewResource {
 
-	@Path("/all")
+	@Path("/")
 	@GET
 	public Response getAll();
 	
